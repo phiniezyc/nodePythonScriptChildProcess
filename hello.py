5c0e1e379be671358d89a325061353fca729b637
Chance Phiniezy <phiniezyc@gmail.com>
2019-08-02 03:19:10 -0400

    Adds better comments

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 import sys
 # Takes first name and last name via command
 # line arguments and then display them
-print("Output from Python")
+print("This Is Output from Python")
 print("First name: " + sys.argv[1])
 print("Last name: " + sys.argv[2])
+
+# Could use this to run a python machine learning or other process in a node app
 
